4ddc0da8848105691dfea644a04e3a8033c0f0b3
Brandon Alonso Salinas <thegeru2prx@gmail.com>
2024-02-03 22:39:37 -0600

    API_menuModv3

--- src/components/Rutas.js
+++ src/components/Rutas.js
@@ -1,20 +1,20 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Error } from "./Error";
 import { Inicio } from "./Inicio";
 import { Menu } from "./Menu";
 import { Productos } from "./Productos";
-    
-export function Rutas(){
-    return(
-        <>
-        <BrowserRouter>
-        <Menu />
-            <Routes>
-                <Route path="*" element={<Error />}></Route>
-                <Route path="/" element={<Inicio />}></Route>
-                <Route path="/productos" element={<Productos />}></Route>
-            </Routes>
-        </BrowserRouter>        
-        </>
-    );
+
+export function Rutas() {
+  return (
+    <>
+      <Menu />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Inicio />}></Route>
+          <Route path="/productos" element={<Productos />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
